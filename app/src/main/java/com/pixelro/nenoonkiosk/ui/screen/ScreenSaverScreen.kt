b48bcd381703bdcd8e35b2bc73a308e53a33dad2
CodingHarang <txepahs@gmail.com>
2023-06-12 13:57:28 +0900

    Update

--- app/src/main/java/com/pixelro/nenoonkiosk/ui/screen/ScreenSaverScreen.kt
+++ app/src/main/java/com/pixelro/nenoonkiosk/ui/screen/ScreenSaverScreen.kt
@@ -2,6 +2,11 @@ package com.pixelro.nenoonkiosk.ui.screen
 
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,13 +21,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withAnnotation
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -32,11 +45,34 @@ import androidx.media3.ui.PlayerView
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.pixelro.nenoonkiosk.NenoonViewModel
 
+@OptIn(ExperimentalTextApi::class)
 @androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
 @Composable
 fun ScreenSaverScreen(
     viewModel: NenoonViewModel
 ) {
+    val transition = rememberInfiniteTransition()
+    val shiftVal by transition.animateFloat(
+        initialValue = 0f, targetValue = 0.5f, animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = 1000
+            },
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val text = buildAnnotatedString {
+        withAnnotation("squiggles", annotation = "ignored") {
+            withStyle(
+                SpanStyle(
+                    color = Color(0xff1d71e1),
+                    baselineShift = BaselineShift(shiftVal)
+                )
+            ) {
+                append("Touch")
+            }
+        }
+        append(" to Start an\nEye Test!")
+    }
     val systemUiController = rememberSystemUiController()
     val context = LocalContext.current
     val exoPlayer = viewModel.exoPlayer
@@ -49,7 +85,7 @@ fun ScreenSaverScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        DisposableEffect(key1 = Unit) {
+        DisposableEffect(true) {
             systemUiController.systemBarsDarkContentEnabled = false
             exoPlayer.play()
             onDispose {
@@ -61,15 +97,23 @@ fun ScreenSaverScreen(
             modifier = Modifier
                 .padding(top = viewModel.statusBarPadding.collectAsState().value.dp)
         )
-        Text(
-            modifier = Modifier
-                .padding(bottom = 20.dp),
-            text = "Touch to Start an\nEye Test!",
-            color = Color(0xffffffff),
-            fontWeight = FontWeight.Bold,
-            fontSize = 80.sp,
-            textAlign = TextAlign.Center
-        )
+        Box() {
+            Column(
+                modifier = Modifier
+                    .height(300.dp),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(bottom = 20.dp),
+                    text = text,
+                    color = Color(0xffffffff),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 80.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
         AndroidView(
             modifier = Modifier
                 .fillMaxWidth()
@@ -88,6 +132,10 @@ fun ScreenSaverScreen(
                     useController = false
                 }
             }
+        )
+        Spacer(
+            modifier = Modifier
+                .height(300.dp)
         )
     }
 }
